a6ec553aefb643ff84b71af7f2230f3016146eb6
Isaque Felix Seabra <isaque.seabra@hotmail.com>
2020-03-11 19:33:47 -0300

    Add project files.

---
+++ ChallengeReply2020/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace ChallengeReply2020
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
